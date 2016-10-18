ab3812d46db78874f621bfb54baa5e01e3a42831
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-18 19:26:12 +0100

    Update javascript.js

--- Clase-2/SweetAlert2/javascript.js
+++ Clase-2/SweetAlert2/javascript.js
@@ -2,8 +2,27 @@ function mensaje() {
   swal({
     title: 'Título',
     text: 'Mensaje de texto',
+    type: 'success',
+    showCloseButton: true,
+    confirmButtonText: '<i class="fa fa-thumbs-up"></i> ¡Bien!',
+    confirmButtonColor: '#1db75a',
+    showCancelButton: true,
+    cancelButtonText: '<i class="fa fa-thumbs-down"></i> :(',
+  }).then(
+    function() { swal('¡Aceptado!') },
+    function() { swal('¡Cancelado!') }
+  );
+}
+
+
+/*function mensaje() {
+  swal({
+    title: 'Título',
+    text: 'Mensaje de texto',
     html: '<p>Suscríbete al <strong>blog</strong>.</p>',
     type: 'success',
     timer: 3000,
   });
-}
+}*/
+
+
