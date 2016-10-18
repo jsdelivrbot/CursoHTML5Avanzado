495f36fdf36a0045e8d619adf032df56a846bc76
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-18 19:17:48 +0100

    Update javascript.js

--- Clase-2/Ouibounce/javascript.js
+++ Clase-2/Ouibounce/javascript.js
@@ -1,5 +1,6 @@
-// Configuramos la librería Ouibounce
-ouibounce(document.getElementById('modal-card'), {
-  aggressive: true,			// borra la cookie viewedOuibounceModal
-  timer: 5						// espera (evita falsos positivos)
+$(document).ready(function() {
+  ouibounce(document.getElementById('modal-card'), {
+    aggressive: true,			// borra la cookie viewedOuibounceModal
+    timer: 5						// espera (evita falsos positivos)
+  });
 });
