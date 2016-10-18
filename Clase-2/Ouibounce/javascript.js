1dc638be56b8775961146ff688fcc7114c86f2f0
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-18 19:05:42 +0100

    Update javascript.js

--- Clase-2/Ouibounce/javascript.js
+++ Clase-2/Ouibounce/javascript.js
@@ -3,7 +3,3 @@ ouibounce(document.getElementById('modal-card'), {
   aggressive: true,			// borra la cookie viewedOuibounceModal
   timer: 5						// espera (evita falsos positivos)
 });
-
-// Opcional (activa a los 5 segundos)
-var modal = ouibounce(...);
-setTimeout(function() { modal.fire() }, 5000);	// muestra a los 5seg
