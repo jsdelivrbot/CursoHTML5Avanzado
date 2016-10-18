6ff0d774e52eef006dc93ed910d1edb9c2e9ae6b
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-18 19:24:24 +0100

    Create javascript.js

---
+++ Clase-2/SweetAlert2/javascript.js
@@ -0,0 +1,9 @@
+function mensaje() {
+  swal({
+    title: 'Título',
+    text: 'Mensaje de texto',
+    html: '<p>Suscríbete al <strong>blog</strong>.</p>',
+    type: 'success',
+    timer: 3000,
+  });
+}
