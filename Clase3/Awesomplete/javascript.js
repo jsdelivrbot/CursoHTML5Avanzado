eb1931570cac9d5066eee362163b196dfa1216ba
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-19 17:14:39 +0100

    Update javascript.js

--- Clase3/Awesomplete/javascript.js
+++ Clase3/Awesomplete/javascript.js
@@ -1,4 +1,4 @@
 var input = document.getElementById('datos');
 new Awesomplete(input, {
-    data-list: ["Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"]
+    list: ["Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"]
 });
