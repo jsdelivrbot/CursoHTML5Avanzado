0da5c8c99313f614d8386d402e038096b726342e
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-19 17:23:08 +0100

    Update javascript.js

--- Clase-2/Acordeon/javascript.js
+++ Clase-2/Acordeon/javascript.js
@@ -1,4 +1,6 @@
-$('.accordion').accordion({
-  transitionSpeed: 400,		// velocidad del acordeón
-  transitionEasing: 'ease'	// ritmo del acordeón (linear, ease-in…)
+$(document).ready(function() {  
+    $('.accordion').accordion({
+    transitionSpeed: 400,		// velocidad del acordeón
+    transitionEasing: 'ease'	// ritmo del acordeón (linear, ease-in…)
+  });
 });
