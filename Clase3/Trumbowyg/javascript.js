8617c9194b19954db79ddf3b9b2fc2268cc1f1e2
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-19 18:48:28 +0100

    Update javascript.js

--- Clase3/Trumbowyg/javascript.js
+++ Clase3/Trumbowyg/javascript.js
@@ -3,5 +3,20 @@ $(document).ready(function() {
   });
 
   $('#trumbowyg').trumbowyg('html'); // Obtienes el HTML generado
+  
+  $('.editor').trumbowyg({
+  lang: 'es', // Idioma (es.min.js incluido)
+  btns: [
+          ['bold', 'italic', 'underline', 'strikethrough'],
+          ['superscript', 'subscript'],
+          ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+          ['unorderedList', 'orderedList'],
+          ['link', 'insertImage'],
+          ['horizontalRule', 'removeformat'],
+          ['viewHTML'],
+          ['fullscreen']
+        ],
+});
+
 
 });
