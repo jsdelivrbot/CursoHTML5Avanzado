896ad7d043dee04a81ae6dd7eb101a8f714b5c3e
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-19 18:52:48 +0100

    Update javascript.js

--- Clase3/Trumbowyg/javascript.js
+++ Clase3/Trumbowyg/javascript.js
@@ -1,23 +1,11 @@
 $(document).ready(function() {
-  $('.editor').trumbowyg({
-    svgPath: '/icons/icons.svg', 
-  });
 
-  $('#trumbowyg').trumbowyg('html'); // Obtienes el HTML generado
-  
   $('.editor').trumbowyg({
-  lang: 'es', // Idioma (es.min.js incluido)
-  btns: [
-          ['bold', 'italic', 'underline', 'strikethrough'],
-          ['superscript', 'subscript'],
-          ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
-          ['unorderedList', 'orderedList'],
-          ['link', 'insertImage'],
-          ['horizontalRule', 'removeformat'],
-          ['viewHTML'],
-          ['fullscreen']
-        ],
-});
+    svgPath: '/icons/icons.svg', // path of icons.svg
+    // Habitualmente, se indica la ruta del SVG en este parámetro, pero
+    // en codepen, simplemente pegaremos el contenido del SVG en la parte
+    // del código HTML
+  });
 
 
 });
