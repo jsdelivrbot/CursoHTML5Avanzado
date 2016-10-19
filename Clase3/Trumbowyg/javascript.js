70cb1cb6bde05dad4db321890d13f286c7d3c2c3
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-19 18:58:22 +0100

    Update javascript.js

--- Clase3/Trumbowyg/javascript.js
+++ Clase3/Trumbowyg/javascript.js
@@ -5,8 +5,20 @@ $(document).ready(function() {
     // Habitualmente, se indica la ruta del SVG en este parámetro, pero
     // en codepen, simplemente pegaremos el contenido del SVG en la parte
     // del código HTML
+    
+    lang: 'es', // Idioma (es.min.js incluido)
+    btns: [
+          ['bold', 'italic', 'underline', 'strikethrough'],
+          ['superscript', 'subscript'],
+          ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+          ['unorderedList', 'orderedList'],
+          ['link', 'insertImage'],
+          ['horizontalRule', 'removeformat'],
+          ['viewHTML'],
+          ['fullscreen']
+        ],
   });
 
-  $('#trumbowyg').trumbowyg('html');
+
 
 });
