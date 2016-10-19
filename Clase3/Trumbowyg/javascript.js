c2153274597dc72cbbccca50acd157a3eea27032
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-19 18:53:25 +0100

    Update javascript.js

--- Clase3/Trumbowyg/javascript.js
+++ Clase3/Trumbowyg/javascript.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   $('.editor').trumbowyg({
-    svgPath: '/icons/icons.svg', // path of icons.svg
+    svgPath: 'https://saraygm.github.io/icons/icons.svg', // path of icons.svg
     // Habitualmente, se indica la ruta del SVG en este parámetro, pero
     // en codepen, simplemente pegaremos el contenido del SVG en la parte
     // del código HTML
