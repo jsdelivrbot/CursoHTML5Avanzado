b533484d4f36e45c3d5843fb2d5fe6fd7a612d65
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-19 19:29:10 +0100

    Update javascript.js

--- Clase3/Waypoints/javascript.js
+++ Clase3/Waypoints/javascript.js
@@ -2,7 +2,7 @@
 $(document).ready(function() { 
   var waypoint = new Waypoint({
     // Elemento objetivo
-    element: document.getElementById('tres'),
+    element: document.getElementById('meta'),
     // Función disparada
     handler: function(direction) {
       alert('¡Has llegado!');
