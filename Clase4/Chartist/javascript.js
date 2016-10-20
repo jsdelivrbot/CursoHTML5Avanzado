04a20a2777ccaf67aa883d179922440294b66ab7
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-20 16:23:23 +0100

    Update javascript.js

--- Clase4/Chartist/javascript.js
+++ Clase4/Chartist/javascript.js
@@ -1,6 +1,6 @@
 $(document).ready(function() { 
   var data = {
-    labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
+    labels: ['USA', 'Spain', 'UK', 'Japan', 'China', 'France'],
     series: [{ data: [5, 4, 6, 11, 10, -5]},
              { data: [2, 0, 18, 2, 7, -9]},
              { data: [3, 10, 4, 6, 6, -7]},
