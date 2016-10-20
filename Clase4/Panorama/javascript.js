609a69c73bd728c1d0e1c8f940f8d82bbd3d9883
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-20 19:40:23 +0100

    Update javascript.js

--- Clase4/Panorama/javascript.js
+++ Clase4/Panorama/javascript.js
@@ -1,6 +1,6 @@
 $(document).ready(function() { 
     var PSV = PhotoSphereViewer({
-    panorama:  'panoramica2.jpg',
+    panorama:  'panoramica3.jpg',
     container: 'photosphere',
     loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
   });
