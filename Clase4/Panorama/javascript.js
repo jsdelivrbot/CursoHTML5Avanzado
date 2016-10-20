1bbf18f956b8e635d5482a7ca5d6c20d71733835
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-20 19:24:53 +0100

    Update javascript.js

--- Clase4/Panorama/javascript.js
+++ Clase4/Panorama/javascript.js
@@ -1,8 +1,8 @@
 $(document).ready(function() { 
-  var PSV = PhotoSphereViewer({
-  panorama:  'http://www.blogdelfotografo.com/wp-content/uploads/2013/02/Fotografia-Panoramica-7.jpg',
-  container: 'photosphere',
-  loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
-});
+    var PSV = PhotoSphereViewer({
+    panorama:  'panoramica.jpg',
+    container: 'photosphere',
+    loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
+  });
 
 });
