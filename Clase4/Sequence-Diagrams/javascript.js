6f3a53d7b00600cf2038cae9fefa2696eb0058b2
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-20 16:53:02 +0100

    Update javascript.js

--- Clase4/Sequence-Diagrams/javascript.js
+++ Clase4/Sequence-Diagrams/javascript.js
@@ -1,6 +1,7 @@
 $(document).ready(function() { 
     // var sample = 'Entrada->Salida: Función';
-    var sample = 'Title: Esto es un título';
+    // var sample = 'Title: Esto es un título';
+    var sample = 'Izquierda-->Derecha: Ir en este sentido';
 
     var diagram = Diagram.parse(sample);
 
