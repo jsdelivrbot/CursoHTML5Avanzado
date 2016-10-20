b6a0ed3baab33dd71f77e34ee6f49b85b6127949
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-20 17:09:49 +0100

    Update javascript.js

--- Clase4/Viz/javascript.js
+++ Clase4/Viz/javascript.js
@@ -1,3 +1,15 @@
 $(document).ready(function() { 
+  var sample = 'digraph g { a -> b; }'; // grafo dirigido
+
+  var options = {
+    format: 'svg'
+    // format: 'png-image-element'
+  }
+
+  var image = Viz(sample, options);
+  var main = document.getElementById('main');
+
+  main.innerHTML = image;        // SVG
+  main.appendChild(image);    // PNG
 
 });
