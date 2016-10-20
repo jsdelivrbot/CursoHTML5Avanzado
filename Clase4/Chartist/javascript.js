b35a5b97076a110548a2199dfef7bde73571ce5f
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-20 16:20:50 +0100

    Update javascript.js

--- Clase4/Chartist/javascript.js
+++ Clase4/Chartist/javascript.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var data = {
     labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
     series: [{ 
-               data: [2, 4, 6, 8, 10, 12]
+               data: [5, 4, 6, 11, 10, -5]
             }]
   };
 
