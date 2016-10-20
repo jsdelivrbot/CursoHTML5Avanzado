6eb9470b12ed3f576a87e80497e08807b21ef81f
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-20 18:51:58 +0100

    Update javascript.js

--- Clase4/Tarjeta3D/javascript.js
+++ Clase4/Tarjeta3D/javascript.js
@@ -1,7 +1,7 @@
 $(document).ready(function() { 
       $('#card').flip({
       axis: 'x',                // 'x' para giro en eje X
-      trigger: 'manual',        // 'hover': giro sobre tarjeta. 'click'
+      trigger: 'hover',        // 'hover': giro sobre tarjeta. 'click'
       speed: 800,                // Velocidad del giro
     });
           
