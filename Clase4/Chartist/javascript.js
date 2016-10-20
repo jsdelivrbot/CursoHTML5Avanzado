5b0cbe5b0e58ed76db6cd4f808b4449d38f48e48
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-20 16:25:34 +0100

    Update javascript.js

--- Clase4/Chartist/javascript.js
+++ Clase4/Chartist/javascript.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     labels: ['USA', 'Spain', 'UK', 'Japan', 'China', 'France'],
     series: [{ data: [5, 4, 6, 11, 10, -5]},
              { data: [2, 0, 18, 2, 7, -9]},
-             { data: [3, 10, 4, 6, 6, -7]},
+             { data: [3, 10, 4, 6, 6, 0]},
             ]
   };
 
