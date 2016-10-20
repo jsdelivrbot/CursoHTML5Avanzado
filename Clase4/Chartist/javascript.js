5949e9d175e5cbcaf62c0498e3626abcce673692
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-20 16:40:16 +0100

    Update javascript.js

--- Clase4/Chartist/javascript.js
+++ Clase4/Chartist/javascript.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
      donut: true,
      donutWidth: 70,
      startAngle: 90,
-     total: 300
+     total: 100
   }
 
   new Chartist.Pie('.chart3', data2, options2);
