f157b35d72686c0167598ee51d202dea444753ab
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-20 16:45:49 +0100

    Create javascript.js

---
+++ Clase4/Sequence-Diagrams/javascript.js
@@ -0,0 +1,9 @@
+$(document).ready(function() { 
+    var sample = 'A->B: Mensaje';
+
+    var diagram = Diagram.parse(sample);
+
+    diagram.drawSVG('diagram', {
+        theme: 'hand'    // o 'simple'
+    });
+});
