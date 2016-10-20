f6727277d3aae578cab2d67e9b61c1d9f6a96873
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-20 16:14:45 +0100

    Create javascript.js

---
+++ Clase4/Chartist/javascript.js
@@ -0,0 +1,15 @@
+$(document).ready(function() { 
+
+
+
+  var data = {
+    labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
+    series: [{ 
+               data: [2, 4, 6, 8, 10, 12]
+            }]
+  };
+
+  var options = {}
+
+  new Chartist.Line('.chart', data, options);
+});
