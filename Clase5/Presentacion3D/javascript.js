f407f71e0c4716a7915de7c16b9ceedb64492ecc
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-21 16:48:37 +0100

    Update javascript.js

--- Clase5/Presentacion3D/javascript.js
+++ Clase5/Presentacion3D/javascript.js
@@ -1,4 +1,9 @@
+$(document).ready(function() { 
+  impress().init();
 
+});
+
+/* En vez de usar el document ready, usamos esto que es sin jQuery: */
 windows.addEventListener('DOMContentLoaded', function() {
     impress().init();
 };
