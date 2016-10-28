87b8fc5f91681cd42f58d440e6a4122fba771027
saraygm <saraygarciamingorance11bbaa@gmail.com>
2016-10-28 19:30:21 +0100

    Create javascript.js

---
+++ Entregar/BeforeAfter/javascript.js
@@ -0,0 +1,25 @@
+$(document).ready (function() { /* Para asegurar la carga total de la página antes de activar las funcionalidades de Javascript */
+	/* Pausar / reproducir audio de la web */
+	var boton_audio = document.getElementById("boton_audio");
+	var audio = document.getElementsByTagName("audio")[0];
+	var sonido = true;
+    boton_audio.addEventListener("click", function() {
+    	if (!sonido) {
+	        audio.play();
+	        this.innerHTML = '<i class="fa fa-volume-up" aria-hidden="true">';
+	        this.className = "button button-circle button-small button-action button-longshadow button-glow";	        
+	        sonido = true;
+    	} else {
+	        audio.pause();
+	        this.innerHTML = '<i class="fa fa-volume-down" aria-hidden="true">';
+	        this.className = "button button-circle button-small button-caution button-longshadow button-glow";
+	        sonido = false;
+    	}
+    });
+
+
+	/* Lanza la comparación de imágenes Before-After */
+	$('.ba-slider').beforeAfter();
+
+
+});
